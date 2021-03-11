3ceb12fbc967c88add51e5de598bc72d18a3f07d
Prathiba <prathiba.sumathikannappan@bam.com>
2021-03-11 22:32:31 +0100

    update initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadingService} from './loading/loading.service';
+import {MessagesService} from './messages/messages.service';
+import {AuthStore} from './services/auth.store';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-angular';
+export class AppComponent implements  OnInit {
+
+    constructor(public auth: AuthStore) {
+
+    }
+
+    ngOnInit() {
+
+
+    }
+
+  logout() {
+        this.auth.logout();
+
+  }
+
 }
